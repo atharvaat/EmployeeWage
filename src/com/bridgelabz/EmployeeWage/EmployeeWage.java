b2efe7f5e00ac144bbbccec184b6c36377f54865
atharva <atharvatodankar.at@gmail.com>
2023-01-06 15:47:57 +0530

    Add:for check Employee is present or Absent

---
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.EmployeeWage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		 double code = Math.floor(Math.random()*10)%2;
+	        if(code==1){
+	            System.out.println("Employee is Present");
+	        }
+	        else {
+	            System.out.println("Employee is Absent");
+
+	}
+
+	}
+}	
