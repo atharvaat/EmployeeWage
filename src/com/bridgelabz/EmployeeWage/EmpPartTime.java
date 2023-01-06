6570429549dea56e337b6f94d6b6fedf3a7aa07a
atharva <atharvatodankar.at@gmail.com>
2023-01-06 15:59:15 +0530

    Add:Adding program Employeeewage PartTime

---
+++ src/com/bridgelabz/EmployeeWage/EmpPartTime.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.EmployeeWage;
+
+public class EmpPartTime {
+
+	public static void main(String[] args) {
+		 int wagePerHour = 20;
+	        int fullTimeHour = 8;
+	        int partTimeHour = 4;
+	        int dailyWage = 0;
+
+	        int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+
+	        if (employeeCheck == 2) {
+	            dailyWage = fullTimeHour * wagePerHour;
+	            System.out.println("Employee is present full time");
+	        } else if (employeeCheck == 1) {
+	            dailyWage = partTimeHour * wagePerHour;
+	            System.out.println("Employee is present half time");
+	        } else {
+	            System.out.println("Employee is absent");
+	        }
+	        System.out.println("Employee total wage is: " +dailyWage);
+	}
+
+}
