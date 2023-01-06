34b090c6a56c0d396c6da37f8c52a4c6f2ea5244
atharva <atharvatodankar.at@gmail.com>
2023-01-06 15:53:37 +0530

    Add:Program for DailyEmployeewage

---
+++ src/com/bridgelabz/EmployeeWage/DailyEmpWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.EmployeeWage;
+
+public class DailyEmpWage {
+
+	public static void main(String[] args) {
+		 int fullyDayHours = 8;
+	        int wagePerHour = 20;
+	        int dailyWage = (fullyDayHours*wagePerHour);
+
+	        System.out.println("employee is present and his daily wage is "+dailyWage);
+	}
+}
